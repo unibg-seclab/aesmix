e65b656e178154f14a6c59f78275c22ff33b4e6f
enricobacis <enrico.bacis@gmail.com>
2016-05-03 16:05:53 +0200

    add lck.h

---
+++ c/lck.h
@@ -0,0 +1,13 @@
+#define BLOCK_SIZE                                 16
+#define MINI_SIZE                                   4
+#define MINI_PER_MACRO                           1024
+#define MINI_PER_BLOCK       (BLOCK_SIZE / MINI_SIZE)
+#define MACRO_SIZE       (MINI_SIZE * MINI_PER_MACRO)
+#define DIGITS           ((int) log2(MINI_PER_MACRO))
+#define DOF              ((int) log2(MINI_PER_BLOCK))
+
+void encrypt_macroblock(unsigned char* macro, unsigned char* out,
+                        unsigned char* key, unsigned char* iv);
+
+void decrypt_macroblock(unsigned char* macro, unsigned char* out,
+                        unsigned char* key, unsigned char* iv);
