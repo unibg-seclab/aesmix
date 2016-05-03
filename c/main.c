11f9999fc8782f32ab4dfa49d440f5c7d909bf83
enricobacis <enrico.bacis@gmail.com>
2016-05-03 17:46:18 +0200

    Encrypt/Decrypt multiple macroblocs in main.c

--- c/main.c
+++ c/main.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include "lck.h"
 
+#define MACROS                       1024
 unsigned char key[] = "SQUEAMISHOSSIFRA";
-unsigned char iv[]  = "IVIVIVIVIVIVIVIV";
 
 void print_hex(const unsigned char *s, unsigned int l)
 {
@@ -15,22 +15,36 @@ void print_hex(const unsigned char *s, unsigned int l)
     printf("\n");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    unsigned char in[MACRO_SIZE], out[MACRO_SIZE], dec[MACRO_SIZE];
+    int i, macros;
+    unsigned char  in[MACRO_SIZE];
+    unsigned char out[MACRO_SIZE];
+    unsigned char dec[MACRO_SIZE];
+    unsigned char  iv[BLOCK_SIZE];
+
+    macros = (argc > 1) ? atoi(argv[1]) : MACROS;
+
+    RAND_pseudo_bytes(iv, BLOCK_SIZE);
     RAND_pseudo_bytes(in, MACRO_SIZE);
+    //printf("PLAINTEXT:\n");
+    //print_hex(in, MACRO_SIZE);
 
-    printf("PLAINTEXT:  ");
-    print_hex(in, MACRO_SIZE);
+    printf("AESLCK-ing %d macroblocks ...\n", macros);
+    for (i=0; i < macros; ++i) {
+        //RAND_pseudo_bytes(iv, BLOCK_SIZE);
 
-    encrypt(in, out, MACRO_SIZE, key, iv);
-    printf("CIPHERTEXT: ");
-    print_hex(out, MACRO_SIZE);
+        encrypt(in, out, MACRO_SIZE, key, iv);
+        //printf("CIPHERTEXT:\n");
+        //print_hex(out, MACRO_SIZE);
 
-    decrypt(out, dec, MACRO_SIZE, key, iv);
-    printf("DECRYPTED:  ");
-    print_hex(dec, MACRO_SIZE);
+        decrypt(out, dec, MACRO_SIZE, key, iv);
+        //printf("DECRYPTED:\n");
+        //print_hex(dec, MACRO_SIZE);
 
-    assert(0 == strncmp((const char*)in, (const char*)dec, MACRO_SIZE));
+        assert(0 == strncmp((const char*)in, (const char*)dec, MACRO_SIZE));
+    }
+
+    printf("DONE\n");
     return 0;
 }
