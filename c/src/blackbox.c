3665ba0a6e23561ec232ce5bd95d01a2f1cdaeb2
enricobacis <enrico.bacis@gmail.com>
2016-05-05 16:42:44 +0200

    Add test to check IV effectiveness

--- c/src/blackbox.c
+++ c/src/blackbox.c
@@ -13,12 +13,15 @@ int main(int argc, char *argv[])
     unsigned char  in2[MACRO_SIZE];
     unsigned char out1[MACRO_SIZE];
     unsigned char out2[MACRO_SIZE];
-    unsigned char   iv[BLOCK_SIZE];
+    unsigned char out3[MACRO_SIZE];
+    unsigned char  iv1[BLOCK_SIZE];
+    unsigned char  iv2[BLOCK_SIZE];
     int i, j;
 
     RAND_pseudo_bytes(in1, MACRO_SIZE);
-    RAND_pseudo_bytes(iv, BLOCK_SIZE);
-    encrypt(in1, out1, MACRO_SIZE, key, iv);
+    RAND_pseudo_bytes(iv1, BLOCK_SIZE);
+    RAND_pseudo_bytes(iv2, BLOCK_SIZE);
+    encrypt(in1, out1, MACRO_SIZE, key, iv1);
 
     for (i=0; i<MACRO_SIZE; i+=MINI_SIZE) {
         printf("CHANGING MINIBLOCK %d  =>  ", i / MINI_SIZE);
@@ -26,11 +29,15 @@ int main(int argc, char *argv[])
         do { RAND_pseudo_bytes(&in2[i], MINI_SIZE); }
         while (0 == memcmp((const char*)in1, (const char*)in2, MACRO_SIZE));
 
-        encrypt(in2, out2, MACRO_SIZE, key, iv);
+        encrypt(in2, out2, MACRO_SIZE, key, iv1);
+        encrypt(in2, out3, MACRO_SIZE, key, iv2);
         for (j=0; j<MACRO_SIZE; j+=BLOCK_SIZE) {
             D printx("out1: ", &out1[j], BLOCK_SIZE);
             D printx("out2: ", &out2[j], BLOCK_SIZE);
+            // test that entropy flows from each block to each other
             assert(0 != memcmp((const char*)&out1[j], (const char*)&out2[j], BLOCK_SIZE));
+            // test that IV affects all the blocks
+            assert(0 != memcmp((const char*)&out2[j], (const char*)&out3[j], BLOCK_SIZE));
         }
         printf("ALL OUTPUT BLOCKS CHANGED\n");
     }
