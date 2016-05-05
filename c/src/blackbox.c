1893fa84297a9a9b49ed44c565ae873405299ab1
enricobacis <enrico.bacis@gmail.com>
2016-05-05 14:55:34 +0200

    - use memcmp instead of strncmp (strncmp is lazy) - use better names - ensure that the random miniblock is different

--- c/src/blackbox.c
+++ c/src/blackbox.c
@@ -9,29 +9,29 @@ unsigned char key[] = "SQUEAMISHOSSIFRA";
 
 int main(int argc, char *argv[])
 {
-    unsigned char   in[MACRO_SIZE];
+    unsigned char  in1[MACRO_SIZE];
     unsigned char  in2[MACRO_SIZE];
-    unsigned char  out[MACRO_SIZE];
+    unsigned char out1[MACRO_SIZE];
     unsigned char out2[MACRO_SIZE];
     unsigned char   iv[BLOCK_SIZE];
     int i, j;
 
-    RAND_pseudo_bytes(in, MACRO_SIZE);
+    RAND_pseudo_bytes(in1, MACRO_SIZE);
     RAND_pseudo_bytes(iv, BLOCK_SIZE);
-    encrypt(in, out, MACRO_SIZE, key, iv);
+    encrypt(in1, out1, MACRO_SIZE, key, iv);
 
     for (i=0; i<MACRO_SIZE; i+=MINI_SIZE) {
-        memcpy(in2, in, MACRO_SIZE);
-        RAND_pseudo_bytes(&in2[i], MINI_SIZE);
-        assert(0 != strncmp((const char*)in, (const char*)in2, MACRO_SIZE));
-        printx("in1: ", in, MACRO_SIZE);
-        printx("in2: ", in2, MACRO_SIZE);
+        printf("\n\n%d", i / MINI_SIZE);
+        memcpy(in2, in1, MACRO_SIZE);
+        do {
+            RAND_pseudo_bytes(&in2[i], MINI_SIZE);
+        } while (0 == memcmp((const char*)in1, (const char*)in2, MACRO_SIZE));
 
         encrypt(in2, out2, MACRO_SIZE, key, iv);
         for (j=0; j<MACRO_SIZE; j+=BLOCK_SIZE) {
-            printx("1: ", &out[j], BLOCK_SIZE);
-            printx("2: ", &out2[j], BLOCK_SIZE);
-            assert(0 != strncmp((const char*)&out[j], (const char*)&out2[j], BLOCK_SIZE));
+            printx("out1: ", &out1[j], BLOCK_SIZE);
+            printx("out2: ", &out2[j], BLOCK_SIZE);
+            assert(0 != memcmp((const char*)&out1[j], (const char*)&out2[j], BLOCK_SIZE));
         }
     }
 
