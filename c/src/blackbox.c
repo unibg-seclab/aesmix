db36c1973b50fdef90e50000c6f40655590ae311
enricobacis <enrico.bacis@gmail.com>
2016-05-06 15:38:26 +0200

    Add test to verify that two identical consequent macroblocks have different ciphertext.

--- c/src/blackbox.c
+++ c/src/blackbox.c
@@ -10,9 +10,9 @@ unsigned char key[] = "SQUEAMISHOSSIFRA";
 int main(int argc, char *argv[])
 {
     unsigned char  in1[MACRO_SIZE];
-    unsigned char  in2[MACRO_SIZE];
+    unsigned char  in2[MACRO_SIZE*2];
     unsigned char out1[MACRO_SIZE];
-    unsigned char out2[MACRO_SIZE];
+    unsigned char out2[MACRO_SIZE*2];
     unsigned char out3[MACRO_SIZE];
     unsigned char  iv1[BLOCK_SIZE];
     unsigned char  iv2[BLOCK_SIZE];
@@ -28,16 +28,19 @@ int main(int argc, char *argv[])
         memcpy(in2, in1, MACRO_SIZE);
         do { RAND_pseudo_bytes(&in2[i], MINI_SIZE); }
         while (0 == memcmp((const char*)in1, (const char*)in2, MACRO_SIZE));
+        memcpy(in2 + MACRO_SIZE, in2, MACRO_SIZE);
 
-        encrypt(in2, out2, MACRO_SIZE, key, iv1);
-        encrypt(in2, out3, MACRO_SIZE, key, iv2);
+        encrypt(in2, out2, MACRO_SIZE*2, key, iv1);
+        encrypt(in2, out3, MACRO_SIZE,   key, iv2);
         for (j=0; j<MACRO_SIZE; j+=BLOCK_SIZE) {
             D printx("out1: ", &out1[j], BLOCK_SIZE, MINI_SIZE);
             D printx("out2: ", &out2[j], BLOCK_SIZE, MINI_SIZE);
             // test that entropy flows from each block to each other
-            assert(0 != memcmp((const char*)&out1[j], (const char*)&out2[j], BLOCK_SIZE));
+            assert(0 != memcmp(out1+j, out2+j, BLOCK_SIZE));
             // test that IV affects all the blocks
-            assert(0 != memcmp((const char*)&out2[j], (const char*)&out3[j], BLOCK_SIZE));
+            assert(0 != memcmp(out2+j, out3+j, BLOCK_SIZE));
+            // test that two identical consequent macroblocks cipher differs
+            assert(0 != memcmp(out2+j, out2+j+MACRO_SIZE, BLOCK_SIZE));
         }
         printf("ALL OUTPUT BLOCKS CHANGED\n");
     }
