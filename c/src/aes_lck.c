c9587db5960c465a455f44521d4cdc655009ebd3
enricobacis <enrico.bacis@gmail.com>
2016-05-06 15:46:11 +0200

    apply DRY principle to code

--- c/src/aes_lck.c
+++ c/src/aes_lck.c
@@ -5,7 +5,6 @@
 #include <math.h>
 #include "aes_lck.h"
 
-
 static inline void do_step_encrypt(EVP_CIPHER_CTX* ctx, unsigned char* macro,
     unsigned char* out, unsigned int step, unsigned char* key, unsigned char* iv
 ){
@@ -106,7 +105,7 @@ static inline void decrypt_macroblock(unsigned char* macro,
     EVP_CIPHER_CTX_cleanup(&ctx);
 }
 
-void encrypt(unsigned char* data, unsigned char* out,
+static inline void process(short enc, unsigned char* data, unsigned char* out,
     unsigned long size, unsigned char* key, unsigned char* iv
 ){
     unsigned char miv[BLOCK_SIZE];
@@ -121,7 +120,8 @@ void encrypt(unsigned char* data, unsigned char* out,
     for (n=0; n < size / MACRO_SIZE; ++n) {
         EVP_EncryptUpdate(&ctx, miv, &outl, (unsigned char*) &n, BLOCK_SIZE);
         D assert(BLOCK_SIZE == outl);
-        encrypt_macroblock(data + n*MACRO_SIZE, out + n*MACRO_SIZE, key, miv);
+        (enc ? encrypt_macroblock : decrypt_macroblock)
+            (data + n*MACRO_SIZE, out + n*MACRO_SIZE, key, miv);
     }
 
     EVP_EncryptFinal(&ctx, miv, &outl);
@@ -129,25 +129,14 @@ void encrypt(unsigned char* data, unsigned char* out,
     EVP_CIPHER_CTX_cleanup(&ctx);
 }
 
+void encrypt(unsigned char* data, unsigned char* out,
+    unsigned long size, unsigned char* key, unsigned char* iv
+){
+    process(1, data, out, size, key, iv);
+}
+
 void decrypt(unsigned char* data, unsigned char* out,
     unsigned long size, unsigned char* key, unsigned char* iv
 ){
-    unsigned char miv[BLOCK_SIZE];
-    unsigned __int128 n;
-    EVP_CIPHER_CTX ctx;
-    int outl;
-
-    D assert(size % MACRO_SIZE == 0);
-    EVP_EncryptInit(&ctx, EVP_aes_128_cbc(), key, iv);
-    EVP_CIPHER_CTX_set_padding(&ctx, 0); // disable padding
-
-    for (n=0; n < size / MACRO_SIZE; ++n) {
-        EVP_EncryptUpdate(&ctx, miv, &outl, (unsigned char*) &n, BLOCK_SIZE);
-        D assert(BLOCK_SIZE == outl);
-        decrypt_macroblock(data + n*MACRO_SIZE, out + n*MACRO_SIZE, key, miv);
-    }
-
-    EVP_EncryptFinal(&ctx, miv, &outl);
-    D assert(0 == outl);
-    EVP_CIPHER_CTX_cleanup(&ctx);
+    process(0, data, out, size, key, iv);
 }
