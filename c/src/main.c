c116d9ae395094803d5130fe85ec3f397d6119fc
enricobacis <enrico.bacis@gmail.com>
2016-05-06 15:37:42 +0200

    use multiple macroblocks to test in DEBUG main.c

--- c/src/main.c
+++ c/src/main.c
@@ -5,37 +5,43 @@
 #include "debug.h"
 #include "aes_lck.h"
 
+#ifdef DEBUG
+#define SIZE  (MACRO_SIZE*4)
+#else
+#define SIZE   MACRO_SIZE
+#endif
+
 unsigned char key[] = "SQUEAMISHOSSIFRA";
 
 int main(int argc, char *argv[])
 {
     int i, macros;
-    unsigned char   in[MACRO_SIZE];
-    unsigned char orig[MACRO_SIZE];
-    unsigned char  out[MACRO_SIZE];
-    unsigned char  dec[MACRO_SIZE];
+    unsigned char   in[SIZE];
+    unsigned char orig[SIZE];
+    unsigned char  out[SIZE];
+    unsigned char  dec[SIZE];
     unsigned char   iv[BLOCK_SIZE];
 
     macros = (argc > 1) ? atoi(argv[1]) : 1;
 
-    RAND_pseudo_bytes(in, MACRO_SIZE);
-    memcpy(orig, in, MACRO_SIZE);
-    D printx("PLAINTEXT: ", in, MACRO_SIZE, MINI_SIZE)
+    RAND_pseudo_bytes(in, SIZE);
+    memcpy(orig, in, SIZE);
+    D printx("PLAINTEXT: ", in, SIZE, MINI_SIZE)
 
-    printf("AESLCK-ing %d macroblocks ...\n", macros);
+    printf("AESLCK-ing %d * %d macroblocks ...\n", SIZE/MACRO_SIZE, macros);
     for (i=0; i < macros; ++i) {
         D RAND_pseudo_bytes(iv, BLOCK_SIZE);
         D printx("IV: ", iv, BLOCK_SIZE, MINI_SIZE);
 
-        encrypt(in, out, MACRO_SIZE, key, iv);
-        D printx("CIPHERTEXT: ", out, MACRO_SIZE, MINI_SIZE);
-        D assert(0 != memcmp((const char*)in, (const char*)out, MACRO_SIZE));
-        D assert(0 == memcmp((const char*)in, (const char*)orig, MACRO_SIZE));
+        encrypt(in, out, SIZE, key, iv);
+        D printx("CIPHERTEXT: ", out, SIZE, MINI_SIZE);
+        D assert(0 != memcmp(in, out, SIZE));
+        D assert(0 == memcmp(in, orig, SIZE));
 
-        D decrypt(out, dec, MACRO_SIZE, key, iv);
-        D printx("DECRYPTED: ", dec, MACRO_SIZE, MINI_SIZE);
-        D assert(0 == memcmp((const char*)in, (const char*)dec, MACRO_SIZE));
-        D assert(0 == memcmp((const char*)in, (const char*)orig, MACRO_SIZE));
+        D decrypt(out, dec, SIZE, key, iv);
+        D printx("DECRYPTED: ", dec, SIZE, MINI_SIZE);
+        D assert(0 == memcmp(in, dec, SIZE));
+        D assert(0 == memcmp(in, orig, SIZE));
     }
 
     printf("DONE\n");
