8a6ea63ca865333ef45fd5a40d1cba6d4d6d8515
enricobacis <enrico.bacis@gmail.com>
2016-05-10 18:17:57 +0200

    use IV as counter directly

--- c/src/aes_lck.c
+++ c/src/aes_lck.c
@@ -100,23 +100,16 @@ static inline void process(const short enc, const unsigned char* data,
     unsigned char* out, const unsigned long size,
     const unsigned char* key, const unsigned char* iv
 ){
-    unsigned char miv[BLOCK_SIZE];
-    unsigned __int128 n;
-    EVP_CIPHER_CTX ctx;
-    int outl;
+    const unsigned char* last = data + size;
+    unsigned __int128 miv;
 
     D assert(0 == size % MACRO_SIZE);
-    EVP_EncryptInit(&ctx, EVP_aes_128_cbc(), key, iv);
-    EVP_CIPHER_CTX_set_padding(&ctx, 0); // disable padding
+    memcpy(&miv, iv, sizeof(miv));
 
-    for (n=0; n < size / MACRO_SIZE; ++n) {
-        EVP_EncryptUpdate(&ctx, miv, &outl, (unsigned char*) &n, BLOCK_SIZE);
-        D assert(BLOCK_SIZE == outl);
+    for ( ; data < last; data+=MACRO_SIZE, out+=MACRO_SIZE, miv++) {
         (enc ? encrypt_macroblock : decrypt_macroblock)
-            (data + n*MACRO_SIZE, out + n*MACRO_SIZE, key, miv);
+            (data, out, key, (unsigned char*) &miv);
     }
-
-    EVP_CIPHER_CTX_cleanup(&ctx);
 }
 
 void encrypt(const unsigned char* data, unsigned char* out,
