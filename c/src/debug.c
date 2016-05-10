6b9d154bf0943b4dc3106fdc50c2c5cbde9c5dc5
enricobacis <enrico.bacis@gmail.com>
2016-05-10 18:22:20 +0200

    remove comparison between signed and unsigned

--- c/src/debug.c
+++ c/src/debug.c
@@ -3,7 +3,7 @@
 
 void print_hex(const unsigned char *s, unsigned int l, unsigned int split)
 {
-    int i;
+    unsigned int i;
     for (i = 0; i < l; ++i) {
         if (i && split && (i % split == 0)) printf("-");
         printf("%02x", (unsigned int) s[i]);
