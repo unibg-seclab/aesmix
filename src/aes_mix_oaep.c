b7ba9c794ecae496d097d2a56b3f9eac849e2a87
enricobacis <enrico.bacis@gmail.com>
2017-06-16 13:02:38 +0200

    Use a dual-mix&slice approach for OAEP
    instead of using a telescopic function, we can just use a bimacroblock
    and shuffle using OAEP with Mix function of Mic&Slice as G (and H)
    functions.
    
    We use a 3 layers OAEP structure

--- src/aes_mix_oaep.c
+++ src/aes_mix_oaep.c
@@ -1,120 +1,110 @@
 #include <openssl/evp.h>
 #include <string.h>
 #include <assert.h>
-
-#include "debug.h"
+#include <math.h>
 
 #include "aes_mix_oaep.h"
 
-static void do_step_encrypt_oaep(
-    EVP_CIPHER_CTX* ctx, unsigned char* buffer, const unsigned char* macro,
-    unsigned char* out, const unsigned long size
+#define BIMACRO_SIZE (2*MACRO_SIZE)
+
+#define SHUFFLE(STEP, OFF, BP, MACRO, BUFFER, FROM, TO)                       \
+    unsigned int j, OFF, mask, start, dist;                                   \
+    unsigned char *BP = buffer;                                               \
+    mask = ((1 << DOF) - 1) << (STEP * DOF);                                  \
+    dist = (1 << (STEP * DOF)) * MINI_SIZE;                                   \
+    for (start=0; start < (1<<DIGITS); start=((start|mask)+1)&~mask) {        \
+        for (j=0, off=start*MINI_SIZE; j < MINI_PER_BLOCK; ++j, off+=dist) {  \
+            memcpy(FROM, TO, MINI_SIZE);                                      \
+            bp += MINI_SIZE;                                                  \
+        }                                                                     \
+    }
+
+static inline void do_step_G(
+    EVP_MD_CTX* ctx, unsigned char* buffer, const unsigned char* macro,
+    unsigned char* gout, const unsigned int step
 ){
-    int outl;
-    unsigned long partsize = size / 2;
-    unsigned char *left = out;
-    unsigned char *right = out + partsize;
-    memcpy(out, macro, size);
+    unsigned int outl;
+    SHUFFLE(step, off, bp, macro, buffer, bp, macro + off);
+    for (off=0; off<MACRO_SIZE; off+=BLOCK_SIZE) {
+        EVP_DigestInit_ex(ctx, EVP_md5(), NULL);
+        EVP_DigestUpdate(ctx, buffer+off, BLOCK_SIZE);
+        EVP_DigestFinal_ex(ctx, gout+off, &outl);
+        D assert(BLOCK_SIZE == outl);
+    }
+}
 
-    D printx("macro:  ", macro, size, BLOCK_SIZE);
+static inline void oaep_G(
+    const unsigned char* macro, unsigned char* gout, unsigned char* buffer
+){
+    int step;
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
 
-    if (partsize == BLOCK_SIZE) {
-        EVP_EncryptUpdate(ctx, buffer, &outl, left, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-        EVP_EncryptUpdate(ctx, buffer, &outl, right, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(left, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-        EVP_EncryptUpdate(ctx, buffer, &outl, left, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-    } else if (partsize > BLOCK_SIZE) {
-        do_step_encrypt_oaep(ctx, buffer + partsize, left, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-        do_step_encrypt_oaep(ctx, buffer + partsize, right, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(left, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-        do_step_encrypt_oaep(ctx, buffer + partsize, left, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, partsize);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, BLOCK_SIZE);
-
-    } else {  // partsize < BLOCK_SIZE
-        printf("plaintext length must be 2*n*16 Bytes (n>0, int)");
+    if ( NULL == ctx ) {
+        printf("Cannot allocate needed memory\n");
         exit(EXIT_FAILURE);
     }
 
-    D printf("partsize: %lu\n", partsize);
-    D printx("out:    ", out, size, BLOCK_SIZE);
+    // Steps 1 -> N
+    for (step=0; step < DIGITS/DOF; ++step) {
+        do_step_G(ctx, buffer, macro, gout, step);
+    }
 
+    EVP_MD_CTX_free(ctx);
 }
 
-static void do_step_decrypt_oaep(
-    EVP_CIPHER_CTX* ctx, unsigned char* buffer, const unsigned char* macro,
-    unsigned char* out, const unsigned long size
+static inline void mixoaep_pad(
+    unsigned char* data, unsigned char* buffer
 ){
-    int outl;
-    unsigned long partsize = size / 2;
-    unsigned char *left = out;
-    unsigned char *right = out + partsize;
-    memcpy(out, macro, size);
+    unsigned char *left = data;
+    unsigned char *right = data + MACRO_SIZE;
+    unsigned char *gout = malloc(MACRO_SIZE);
 
-    D printx("macro:  ", macro, size, BLOCK_SIZE);
-
-    if (partsize == BLOCK_SIZE) {
-        EVP_EncryptUpdate(ctx, buffer, &outl, left, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-        EVP_EncryptUpdate(ctx, buffer, &outl, right, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(left, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-        EVP_EncryptUpdate(ctx, buffer, &outl, left, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-    } else if (partsize > BLOCK_SIZE) {
-        do_step_decrypt_oaep(ctx, buffer + partsize, left, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-        do_step_decrypt_oaep(ctx, buffer + partsize, right, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(left, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-        do_step_decrypt_oaep(ctx, buffer + partsize, left, buffer, partsize);
-        D printx("buffer: ", buffer, partsize, MINI_SIZE);
-        memxor(right, buffer, partsize);
-        D printx("out:    ", out, size, MINI_SIZE);
-
-    } else {  // partsize < BLOCK_SIZE
-        printf("plaintext length must be 2*n*16 Bytes (n>0, int)");
+    if ( !gout ) {
+        printf("Cannot allocate needed memory\n");
         exit(EXIT_FAILURE);
     }
 
-    D printf("partsize: %lu\n", partsize);
-    D printx("out:    ", out, size, BLOCK_SIZE);
+    oaep_G(left, gout, buffer);
+    memxor(right, gout, MACRO_SIZE);
+
+    oaep_G(right, gout, buffer);
+    memxor(left, gout, MACRO_SIZE);
+
+    oaep_G(left, gout, buffer);
+    memxor(right, gout, MACRO_SIZE);
+
+    free(gout);
 }
 
-static inline void mixencrypt_oaep_macroblock (
-    const unsigned char* macro, unsigned char* out, unsigned char* buffer,
+// the function below is identical to mixoaep_pad for odd number of
+// OAEP steps, but must be changed in case of even steps.
+static inline void mixoaep_unpad(
+    unsigned char* data, unsigned char* buffer
+){
+    unsigned char *left = data;
+    unsigned char *right = data + MACRO_SIZE;
+    unsigned char *gout = malloc(MACRO_SIZE);
+
+    if ( !gout ) {
+        printf("Cannot allocate needed memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    oaep_G(left, gout, buffer);
+    memxor(right, gout, MACRO_SIZE);
+
+    oaep_G(right, gout, buffer);
+    memxor(left, gout, MACRO_SIZE);
+
+    oaep_G(left, gout, buffer);
+    memxor(right, gout, MACRO_SIZE);
+
+    free(gout);
+}
+
+
+static inline void mixencrypt_bimacroblock_oaep(
+    const unsigned char* bimacro, unsigned char* out, unsigned char* buffer,
     const unsigned char* key, const unsigned char* iv
 ){
     int outl;
@@ -125,22 +115,24 @@ static inline void mixencrypt_oaep_macroblock (
         exit(EXIT_FAILURE);
     }
 
-    EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key, iv); // init encryption context
-    EVP_CIPHER_CTX_set_padding(ctx, 0);                       // disable padding
+    // add IV
+    memcpy(out, bimacro, BIMACRO_SIZE);
+    memxor(out, iv, BLOCK_SIZE);
 
-//    memxor((unsigned char*) macro, iv, BLOCK_SIZE);           // add IV to input
-    do_step_encrypt_oaep(ctx, buffer, macro, out, MACRO_SIZE);   // oaep shuffle
-//    memxor((unsigned char*) macro, iv, BLOCK_SIZE);      // remove IV from input
+    // OAEP pad
+    mixoaep_pad(out, buffer);
 
-//    EVP_EncryptUpdate(ctx, out, &outl, out, MACRO_SIZE); // post-shuffle encrypt
-//    D assert(MACRO_SIZE == outl);
-
-    EVP_CIPHER_CTX_cleanup(ctx);                              // cleanup context
+    // encrypt
+    EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key, iv);
+    EVP_CIPHER_CTX_set_padding(ctx, 0); // disable padding
+    EVP_EncryptUpdate(ctx, out, &outl, out, BIMACRO_SIZE);
+    D assert(BIMACRO_SIZE == outl);
+    EVP_CIPHER_CTX_cleanup(ctx);
     EVP_CIPHER_CTX_free(ctx);
 }
 
-static inline void mixdecrypt_oaep_macroblock(
-    const unsigned char* macro, unsigned char* out, unsigned char* buffer,
+static inline void mixdecrypt_bimacroblock_oaep(
+    const unsigned char* bimacro, unsigned char* out, unsigned char* buffer,
     const unsigned char* key, const unsigned char* iv
 ){
     int outl;
@@ -151,27 +143,52 @@ static inline void mixdecrypt_oaep_macroblock(
         exit(EXIT_FAILURE);
     }
 
-    EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key, iv); // init decryption context
-    EVP_CIPHER_CTX_set_padding(ctx, 0);                       // disable padding
-
-//    EVP_DecryptUpdate(ctx, out, &outl, macro, MACRO_SIZE); // preshuffle decrypt
-//    D assert(MACRO_SIZE == outl);
-
-    do_step_decrypt_oaep(ctx, buffer, macro, out, MACRO_SIZE); // oaep unshuffle
-//    memxor(out, iv, BLOCK_SIZE);                        // remove IV from output
-
-    EVP_CIPHER_CTX_cleanup(ctx);                              // cleanup context
+    // decrypt
+    EVP_DecryptInit(ctx, EVP_aes_128_ecb(), key, iv);
+    EVP_CIPHER_CTX_set_padding(ctx, 0); // disable padding
+    EVP_DecryptUpdate(ctx, out, &outl, bimacro, BIMACRO_SIZE);
+    D assert(BIMACRO_SIZE == outl);
+    EVP_CIPHER_CTX_cleanup(ctx);
     EVP_CIPHER_CTX_free(ctx);
+
+    // OAEP unpad
+    mixoaep_unpad(out, buffer);
+
+    // remove IV
+    memxor(out, iv, BLOCK_SIZE);
+}
+
+inline void mixbiprocess(
+    mixfn fn, const unsigned char* data, unsigned char* out,
+    const unsigned long size, const unsigned char* key, const unsigned char* iv
+){
+    const unsigned char* last = data + size;
+    unsigned __int128 miv;
+    unsigned char* buffer = malloc(MACRO_SIZE);
+
+    if ( !buffer ) {
+        printf("Cannot allocate needed memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    assert(0 == size % BIMACRO_SIZE);
+    memcpy(&miv, iv, sizeof(miv));
+
+    for ( ; data < last; data+=BIMACRO_SIZE, out+=BIMACRO_SIZE, miv++) {
+        fn(data, out, buffer, key, (unsigned char*) &miv);
+    }
+
+    free(buffer);
 }
 
 void mixencrypt_oaep(const unsigned char* data, unsigned char* out,
     const unsigned long size, const unsigned char* key, const unsigned char* iv
 ){
-    mixprocess(mixencrypt_oaep_macroblock, data, out, size, key, iv);
+    mixbiprocess(mixencrypt_bimacroblock_oaep, data, out, size, key, iv);
 }
 
 void mixdecrypt_oaep(const unsigned char* data, unsigned char* out,
     const unsigned long size, const unsigned char* key, const unsigned char* iv
 ){
-    mixprocess(mixdecrypt_oaep_macroblock, data, out, size, key, iv);
+    mixbiprocess(mixdecrypt_bimacroblock_oaep, data, out, size, key, iv);
 }
