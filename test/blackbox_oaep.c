e1776def1365fc7d053c1d0ca77c416ee7271fc2
enricobacis <enrico.bacis@gmail.com>
2017-06-19 16:27:58 +0200

    adapt blackbox test to oaep

--- test/blackbox_oaep.c
+++ test/blackbox_oaep.c
@@ -9,30 +9,30 @@ unsigned char key[] = "SQUEAMISHOSSIFRA";
 
 int main()
 {
-    unsigned char  in1[MACRO_SIZE];
-    unsigned char  in2[MACRO_SIZE*2];
-    unsigned char out1[MACRO_SIZE];
-    unsigned char out2[MACRO_SIZE*2];
-    unsigned char out3[MACRO_SIZE];
+    unsigned char  in1[BIMACRO_SIZE];
+    unsigned char  in2[BIMACRO_SIZE*2];
+    unsigned char out1[BIMACRO_SIZE];
+    unsigned char out2[BIMACRO_SIZE*2];
+    unsigned char out3[BIMACRO_SIZE];
     unsigned char  iv1[BLOCK_SIZE];
     unsigned char  iv2[BLOCK_SIZE];
     int i, j;
 
-    RAND_bytes(in1, MACRO_SIZE);
+    RAND_bytes(in1, BIMACRO_SIZE);
     RAND_bytes(iv1, BLOCK_SIZE);
     RAND_bytes(iv2, BLOCK_SIZE);
-    mixencrypt_oaep(in1, out1, MACRO_SIZE, key, iv1);
+    mixencrypt_oaep(in1, out1, BIMACRO_SIZE, key, iv1);
 
-    for (i=0; i<MACRO_SIZE; i+=MINI_SIZE) {
-        printf("CHANGING MINIBLOCK %d  =>  ", i / MINI_SIZE);
-        memcpy(in2, in1, MACRO_SIZE);
+    for (i=0; i<BIMACRO_SIZE; i+=MINI_SIZE) {
+        printf("CHANGING MINIBLOCK %d\n", i / MINI_SIZE);
+        memcpy(in2, in1, BIMACRO_SIZE);
         do { RAND_bytes(&in2[i], MINI_SIZE); }
-        while (0 == memcmp((const char*)in1, (const char*)in2, MACRO_SIZE));
-        memcpy(in2 + MACRO_SIZE, in2, MACRO_SIZE);
+        while (0 == memcmp((const char*)in1, (const char*)in2, BIMACRO_SIZE));
+        memcpy(in2 + BIMACRO_SIZE, in2, BIMACRO_SIZE);
 
-        mixencrypt_oaep(in2, out2, MACRO_SIZE*2, key, iv1);
-        mixencrypt_oaep(in2, out3, MACRO_SIZE,   key, iv2);
-        for (j=0; j<MACRO_SIZE; j+=BLOCK_SIZE) {
+        mixencrypt_oaep(in2, out2, BIMACRO_SIZE*2, key, iv1);
+        mixencrypt_oaep(in2, out3, BIMACRO_SIZE,   key, iv2);
+        for (j=0; j<BIMACRO_SIZE; j+=BLOCK_SIZE) {
             D printx("out1: ", &out1[j], BLOCK_SIZE, MINI_SIZE);
             D printx("out2: ", &out2[j], BLOCK_SIZE, MINI_SIZE);
             // test that entropy flows from each block to each other
@@ -40,7 +40,7 @@ int main()
             // test that IV affects all the blocks
             assert(0 != memcmp(out2+j, out3+j, BLOCK_SIZE));
             // test that two identical consequent macroblocks cipher differs
-            assert(0 != memcmp(out2+j, out2+j+MACRO_SIZE, BLOCK_SIZE));
+            assert(0 != memcmp(out2+j, out2+j+BIMACRO_SIZE, BLOCK_SIZE));
         }
         printf("ALL OUTPUT BLOCKS CHANGED\n");
     }
