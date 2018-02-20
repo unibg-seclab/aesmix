6a374555ff763cea12bd1ec32469cf6d53d52a7c
enricobacis <enrico.bacis@gmail.com>
2018-02-20 18:40:48 +0100

    add print_diff to find difference in binary strings

--- includes/debug.h
+++ includes/debug.h
@@ -1,9 +1,19 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 #define printx(str, data, size, split) \
     do { printf(str); print_hex(data, size, split); } while (0);
 
 void print_hex(const unsigned char *s, unsigned int l, unsigned int split);
+
+void print_diff(const unsigned char *s1, const unsigned char *s2, unsigned long size);
 
 #endif // DEBUG_H
--- src/debug.c
+++ src/debug.c
@@ -10,3 +10,14 @@ void print_hex(const unsigned char *s, unsigned int l, unsigned int split)
     }
     printf("\n");
 }
+
+void print_diff(const unsigned char *s1, const unsigned char *s2, unsigned long size)
+{
+    unsigned int i;
+    for (i = 0; i < size; i++) {
+        printf("%02x", (unsigned char) s1[i]);
+        printf((s1[i] == s2[i]) ? ANSI_COLOR_GREEN : ANSI_COLOR_RED);
+        printf("%02x", (unsigned char) s2[i]);
+        printf(ANSI_COLOR_RESET);
+    }
+}
