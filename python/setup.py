2595a109a295e63e22cee64ff918b93f5dddbd5b
enricobacis <enrico.bacis@gmail.com>
2019-03-20 12:30:55 +0100

    add setup script

---
+++ python/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fp:
+    install_requires = [el.strip() for el in fp.readlines() if el.strip()]
+
+setup(name='aesmix',
+      version='1.0',
+      description='Mix&Slice',
+      url='http://github.com/unibg-seclab/aesmix',
+      author='Unibg Seclab',
+      author_email='seclab@unibg.it',
+      license='MIT',
+      zip_safe=False,
+      packages=['aesmix'],
+      cffi_modules=['build_aesmix.py:ffibuilder'],
+      setup_requires=['cffi'],
+      install_requires=install_requires)
