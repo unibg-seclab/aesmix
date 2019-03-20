55d20393bbbb47ed3f5ce00df05060cdf1bad016
enricobacis <enrico.bacis@gmail.com>
2019-03-20 13:20:02 +0100

    add multi-threaded example

--- python/example.py
+++ python/example.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 
-import aesmix
+from aesmix import mixencrypt, mixdecrypt, t_mixencrypt, t_mixdecrypt
 
-plaintext = b"d" * 4096
-key = b"k" * 16
-iv = b"i" * 16
 
-print(b"plaintext: " + plaintext[:64] + b" ... " + plaintext[-64:])
+def test_single_thread():
+    print("\n\nTest single thread")
+    key = b"k" * 16
+    iv = b"i" * 16
 
-ciphertext = aesmix.mixencrypt(plaintext, key, iv, to_string=True)
-print(b"ciphertext: " + ciphertext[:64] + b" ... " + ciphertext[-64:])
+    plaintext = b"d" * 4096
+    print(b"plaintext: " + plaintext[:64] + b" ... " + plaintext[-64:])
 
-decrypted = aesmix.mixdecrypt(ciphertext, key, iv, to_string=True)
-print(b"decrypted: " + decrypted[:64] + b" ... " + decrypted[-64:])
+    ciphertext = mixencrypt(plaintext, key, iv, to_string=True)
+    print(b"ciphertext: " + ciphertext[:64] + b" ... " + ciphertext[-64:])
+
+    decrypted = mixdecrypt(ciphertext, key, iv, to_string=True)
+    print(b"decrypted: " + decrypted[:64] + b" ... " + decrypted[-64:])
+
+
+def test_multi_thread():
+    print("\n\nTest multi thread")
+    key = b"k" * 16
+    iv = b"i" * 16
+    threads = 8
+
+    plaintext = b"d" * (2 ** 30)  # 1 GiB
+    print(b"plaintext: " + plaintext[:64] + b" ... " + plaintext[-64:])
+
+    ciphertext = t_mixencrypt(plaintext, key, iv, threads, to_string=True)
+    print(b"ciphertext: " + ciphertext[:64] + b" ... " + ciphertext[-64:])
+
+    decrypted = t_mixdecrypt(ciphertext, key, iv, threads, to_string=False)
+    print(b"decrypted: " + decrypted[:64] + b" ... " + decrypted[-64:])
+
+
+if __name__ == "__main__":
+    test_single_thread()
+    test_multi_thread()
