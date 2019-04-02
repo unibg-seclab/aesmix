f24b526b1ac4bd80209984608537266e6296608f
enricobacis <enrico.bacis@gmail.com>
2019-04-02 13:26:00 +0200

    add lib files to package

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as README:
     long_description = long_description[long_description.index('Description'):]
 
 setup(name='aesmix',
-      version='1.0',
+      version='1.1',
       description='Mix&Slice',
       long_description=long_description,
       url='http://github.com/unibg-seclab/aesmix',
@@ -13,6 +13,7 @@ setup(name='aesmix',
       author_email='seclab@unibg.it',
       license='MIT',
       zip_safe=False,
+      include_package_data=True,
       packages=['aesmix'],
       cffi_modules=['lib/build_aesmix.py:ffibuilder'],
       scripts=['scripts/mixslice'],
