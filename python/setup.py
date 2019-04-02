eb9b6117f949e1940ebdd168c49350976cc530ff
enricobacis <enrico.bacis@gmail.com>
2019-04-02 10:59:20 +0200

    add long description to setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,8 +8,9 @@ with open('requirements.txt', 'r') as fp:
     install_requires = [el.strip() for el in fp.readlines() if el.strip()]
 
 setup(name='aesmix',
-      version='1.0',
+      version='0.1',
       description='Mix&Slice',
+      long_description=long_description,
       url='http://github.com/unibg-seclab/aesmix',
       author='Unibg Seclab',
       author_email='seclab@unibg.it',
