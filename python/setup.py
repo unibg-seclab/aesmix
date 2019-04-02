6381e76f55c854b24c63ac18940100e771903bd8
enricobacis <enrico.bacis@gmail.com>
2019-04-02 14:15:37 +0200

    bump 1.13

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as README:
     long_description = long_description[long_description.index('Description'):]
 
 setup(name='aesmix',
-      version='1.2',
+      version='1.3',
       description='Mix&Slice',
       long_description=long_description,
       url='http://github.com/unibg-seclab/aesmix',
